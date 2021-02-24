f10215ff4ca5f7a950cf5fd971bd15952814f4ce
clelia <clelia.burle@ynov.com>
2021-02-24 10:56:50 +0100

    set show mine count

--- main.py
+++ main.py
@@ -50,18 +50,43 @@ def game_init():
 
 
 def level_init():
-    global nb_col, nb_lig, nb_mines
-    niveau = choix.get()
-    if niveau == 1:
+    global nb_col, nb_lines, nb_mines
+    level = choix.get()
+    if level == 1:
         nb_col, nb_lig, nb_mines = 10, 10, 12# niveau avancé el
-    if niveau == 2:
+    if level == 2:
         nb_col, nb_lig, nb_mines = 15, 15, 30# niveau expert
     else:
         nb_col, nb_lig, nb_mines = 20, 20, 50#
     canvas.configure(width=(nb_col*dim)+gap, height=(nb_lig*dim)+gap)
     game_init()
 
-nb_col, nb_lines, nb_mines = 0,0,0
+def show_counter():
+    mine_count.configure(text=str(nb_hidden_mines))
+    mine_count.configure(text=str((nb_col*nb_lines)-nb_seen_mines))
+
+
+def show_nb_mines(nb_neighboring_mines, col, lines):
+    global nb_hidden_mines, nb_seen_mines
+    if tab_j[col, lines] == "":
+        nb_seen_mines = nb_seen_mines + 1
+        if tab_j[col, lines] == "d":
+            nb_hidden_mines = nb_hidden_mines + 1
+            nb_seen_mines = nb_seen_mines - 1
+            show_counter()
+            tab_j[col, lines] = nb_neighboring_mines
+            canvas.create_rectangle((col-1)*dim+gap+3, (lines-1)*dim+gap+3, col*dim+gap-3,
+                                    lines*dim+gap-3, width=0, fill="ivory")
+            color = ['blue', 'orange', 'red', 'green', 'cyan', 'skyblue', 'pink']
+            canvas.create_text(col*dim-dim//2+gap, lines*dim-dim//2+gap, text=str(nb_neighboring_mines),
+                               fill=color[nb_neighboring_mines-1], font='Arial 22')
+
+
+fen = Tk()
+fen.title("Démineur")
+fen.resizable(width=False, height=False)
+
+nb_col, nb_lines, nb_mines = 0, 0, 0
 dim, gap, nb_seen_mines = 30, 3, 0
 
 tab_m = {}
@@ -70,5 +95,10 @@ tab_j = {}
 root = tkinter.Tk()
 canvas = Canvas(root, width=10, height=10, bg="black")
 
-choix=IntVar()
+choix = IntVar()
 choix.set(1)
+
+f3 = Frame(fen)
+
+mine_count = Label(f3, text="100")
+mine_count.grid(row=4, column=2, sticky='NE')
